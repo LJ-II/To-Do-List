45b9b658b967a0bd0b3c15ad3ad04fe66733612b
LuK <lucas.tladi@outlook.com>
2025-01-14 15:58:02 +0200

    latest

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -1,4 +1,13 @@
-export async function getAllTodos(req, res, next) {}
+import { connectToDB } from "../util/connect.js";
+import Todo from "../models/todoModel.js";
+import { createError } from "../util/error.js";
+
+export async function getAllTodos(req, res, next) 
+{
+    await connectToDB();
+    const todos = await Todo.find({ userID: req.user.id });
+    res.status(200).send(todos);
+}
 
 export async function getTodo(req, res, next) {}
 
@@ -6,4 +15,15 @@ export async function updateTodo(req, res, next) {}
 
 export async function deleteTodo(req, res, next) {}
 
-export async function addTodo(req, res, next) {}
+export async function addTodo(req, res, next) 
+{
+    console.log(req.body);
+    if (!req.body || !req.body.title)
+    {
+        return next(createError(404, "Title is required!"))
+    }
+
+    await connectToDB();
+    const newTodo = new Todo({ title: req.body.title, userID: req.user.id });
+    res.send(newTodo);
+}
