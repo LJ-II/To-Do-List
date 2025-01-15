aeabfa334c7c1971c6c322dc4bf84a64be0854eb
LuK <lucas.tladi@outlook.com>
2025-01-15 16:13:51 +0200

    third commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
 
 const Login = () =>
 {
-    return <div>Login Page</div>;  
+    return (
+        <div className="h-screen flex justify-center items-center transform -translate-y-16">
+            <form action="" className="flex flex-col gap-6 max-w-xl w-full px-8">
+                <div className="flex flex-col gap-2">
+                <Label>Email:</Label>
+                <Input type="email" name="email" placeholder="Enter your email"/>
+                </div>
+
+                <div className="flex flex-col gap-2">
+                <Label>Password:</Label>
+                <Input type="password" name="password" placeholder="Enter your password"/>
+                </div>
+                <Button>Login</Button>
+                <span className="text-[#63657b] text-center">
+                    Don't have an account? {" "}
+                    <Link to = '/register' className="transition ease-in-out hover:cursor-pointer hover:text-primary hover:underline">
+                         Register
+                    </Link>
+                </span>
+            </form>
+        </div>
+    );
 };
 
 export default Login;
