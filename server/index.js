e5867f25a480c4eda4003e8a803d5376b58ea416
LuK <lucas.tladi@outlook.com>
2025-01-15 10:52:39 +0200

    first commit

--- server/index.js
+++ server/index.js
@@ -4,11 +4,20 @@ import ToDoRoute from "./routes/todo.js";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 dotenv.config();
+
+const corsOptions = 
+{
+    origin: "http://localhost:5173",
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/api/user", AuthRoute);
