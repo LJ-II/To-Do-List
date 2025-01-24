720524d61c7865f08d2d01e27376aac295785e26
LuK <lucas.tladi@outlook.com>
2025-01-24 14:43:56 +0200

    latest

--- server/client2/src/components/Login.jsx
+++ server/client2/src/components/Login.jsx
@@ -36,6 +36,7 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { login } from "@/actions/userActions";
 
+
 const Login = () =>
 {
     const navigate = useNavigate();
@@ -60,7 +61,7 @@ const Login = () =>
 
     console.log(formData);
     
-    /*return(
+    return(
         <div className="h-screen flex justify-center items-center transform -translate-y-16">
             <h1 className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 font-bold text-4xl text-center mb-4 text-transparent bg-clip-text">Welcome to the Todo App</h1>
             <form action={formAction} className="flex flex-col gap-6 max-w-xl w-full px-8">
@@ -85,56 +86,8 @@ const Login = () =>
                 </span>
             </form>
         </div>
-    );*/
+    );
 
-    return (
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <div className="flex-grow flex justify-center items-center transform -translate-y-16">
-            <div className="w-full max-w-xl px-8">
-              <h1 className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 font-bold text-4xl text-center mb-4 text-transparent bg-clip-text">
-                Welcome to the Todo App
-              </h1>
-              <form action={formAction} className="flex flex-col gap-6">
-                <div className="flex flex-col gap-2">
-                  <Label>Email</Label>
-                  <Input
-                    type="email"
-                    name="email"
-                    placeholder="Enter your email"
-                    value={formData.email}
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <Label>Password</Label>
-                  <Input
-                    type="password"
-                    name="password"
-                    placeholder="Enter your password"
-                    value={formData.password}
-                    onChange={handleChange}
-                  />
-                </div>
-                {state.error && <span className="message">{state.error}</span>}
-                <Button disabled={isPending}>
-                  {isPending ? "Logging in" : "Login"}
-                </Button>
-                <span className="text-[#63657b] text-center">
-                  Don't have an account?{" "}
-                  <Link
-                    to="/register"
-                    className="transition ease-in-out hover:cursor-pointer hover:text-primary hover:underline"
-                  >
-                    Register
-                  </Link>
-                </span>
-              </form>
-            </div>
-          </div>
-          <Footer />
-        </div>
-      );
 };
 
 export default Login;
